d74ef13060fe9f1fb6af96cc2bbf91854f5a8275
ankitgit5309 <109146182+ankitgit5309@users.noreply.github.com>
2023-01-15 18:36:46 +0530

    Add files via upload

--- 01_practice_02.c
+++ 01_practice_02.c
@@ -1,19 +1,36 @@
-// Ankit Raj |Area of circle and volume of a cylinder|Date.rev:23-11-2022
-#include<stdio.h>
+// #include<cs50.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
+// int main(int argc, char const *argv[])
+// {
+//     printf("hello world");
+//     return 0;
+// }
+#include <stdio.h> 
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int, int, int, int);
+
+int max_of_four(int a, int b, int c, int d)
 {
-    int radius, height;
-    float pi=3.14;
-    printf("What is the radius of circle\t");
-    scanf("%d",&radius); //Upar me jo data denge ye usko scan karne ke lie 
-    //%d agar integral value hoga tab ke lie 
-    printf("The area of circle is %f\n", pi*radius*radius); //%f ka matlab koi bhi floating point no 
+  int max = a;
+  if (max < b)
+    max = b;
+  if (max < c)
+    max = c;
+  if (max < d)
+    max = d;
 
-    printf("What is the radius of your cylinder\t");
-    scanf("%d",&radius);
-    printf("What is the height of your Cylinder\t");
-    scanf("%d",&height);
-    printf("Volume of cylinder is %f Meter cube",pi*radius*radius*height);
-    return 0;
+  return max ;
+}
+
+int main()
+{
+  int a, b, c, d;
+  scanf("%d %d %d %d", &a, &b, &c, &d);
+  int ans = max_of_four(a, b, c, d);
+  printf("%d", ans);
+
+  return 0;
 }
